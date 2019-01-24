052594eef77cef596434875ade0a0abe8f5c6c97
Miss Araquel <missaraquel@gmail.com>
2019-01-24 13:21:37 -0800

    adding public routes to get all profiles,profile by handle and profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,6 +37,61 @@ router.get(
   }
 );
 
+// @route  GET /api/profile/all
+// @desc   Get all profiles
+// @access public
+router.get("/all", (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
+// @route  GET /api/profile/handle/:handle-backend route
+// @desc   Get profile by handle
+// @access public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route  GET /api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "there is no profile for this user" })
+    );
+});
+
 // @route  POST /api/profile
 // @desc   Create or edit user profile
 // @access pvt
@@ -57,10 +112,8 @@ router.post(
     if (req.body.company) profileFields.company = req.body.company;
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bioo) profileFields.bio = req.body.bio;
+    if (req.body.bio) profileFields.bio = req.body.bio;
     if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername)
-      profileFields.githubusername = req.body.githubusername;
     //skills - split into arr
     if (typeof req.body.skills !== "undefined") {
       profileFields.skills = req.body.skills.split(",");
@@ -72,6 +125,8 @@ router.post(
     if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
 
     Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
