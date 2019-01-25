266d53095b11b122df47b8fd19b7df12426128a3
Miss Araquel <missaraquel@gmail.com>
2019-01-25 13:01:17 -0800

    adding delete route for user experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -220,4 +220,28 @@ router.post(
   }
 );
 
+// @route  DELETE /api/profile/experience/:exp_id
+// @desc   delete exp from profile
+// @access pvt
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //get rm index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        //splice out of arr
+        profile.experience.splice(removeIndex, 1);
+
+        //save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
