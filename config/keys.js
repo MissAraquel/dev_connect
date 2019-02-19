26b6fd227c8e6522f2f70e988f1f6d80c8fff540
Miss Araquel <missaraquel@gmail.com>
2019-02-19 15:17:12 -0800

    prepare for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://jennifer:jennifer2@ds035177.mlab.com:35177/devconnector2",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
